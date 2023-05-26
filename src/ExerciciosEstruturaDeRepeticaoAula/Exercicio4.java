1b82193ed8b8bc6efedaa590e6d11c7450a6a5d4
guiijanuario <guilherme.januario@zup.com.br>
2023-05-26 17:25:40 -0300

    Exercicio 4 finalizado - Exercicios da Aula

---
+++ src/ExerciciosEstruturaDeRepeticaoAula/Exercicio4.java
@@ -0,0 +1,28 @@
+package ExerciciosEstruturaDeRepeticaoAula;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("[------ Programa para fazer a média -------]");
+        System.out.println("[------ Digite a quantidade de números que você quer fazer a média -------]");
+        int qtdNumeros = sc.nextInt();
+
+        int soma = 0;
+        int i = 1;
+
+        while (i <= qtdNumeros){
+            System.out.println("Digite o numero " + i + ":");
+            double numero = sc.nextDouble();
+            soma += numero;
+            i++;
+        }
+
+        double media = (double) soma / qtdNumeros;
+
+        System.out.println("A média é: " + media);
+    }
+}
