a08a585af95b65ec13879d370187fb7fb3c18f68
guiijanuario <guilherme.januario@zup.com.br>
2023-05-26 16:14:23 -0300

    Exericicio 2 da aula finalizado

---
+++ src/ExerciciosEstruturaDeRepeticaoAula/Exercicio2.java
@@ -0,0 +1,9 @@
+package ExerciciosEstruturaDeRepeticaoAula;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        for(int i = 100; i >= 1; i--){
+            System.out.println("Mostrando na tela a sequência: " + i);
+        }
+    }
+}
