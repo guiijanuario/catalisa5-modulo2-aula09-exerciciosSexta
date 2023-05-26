a8f872b0596b5f40b55067d867883ed8eed43102
guiijanuario <guilherme.januario@zup.com.br>
2023-05-26 20:29:00 -0300

    Enviando o exercicio 5 (não existe) - Estrutura de Repetição - Entregar

---
+++ src/ExerciciosEstruturaDeRepeticaoEntregar/Exercicio5.java
@@ -0,0 +1,7 @@
+package ExerciciosEstruturaDeRepeticaoEntregar;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        System.out.println("Erro 404 Not Found - Esse exercício não existe]");
+    }
+}
