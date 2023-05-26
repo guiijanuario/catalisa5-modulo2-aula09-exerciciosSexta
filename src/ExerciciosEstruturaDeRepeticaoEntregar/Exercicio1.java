6250dcb4dea8b63743656aa680cac6edd7e34d41
guiijanuario <guilherme.januario@zup.com.br>
2023-05-26 17:38:47 -0300

    Exercicio 1 realizado - Exercicios de Repetição entregar

---
+++ src/ExerciciosEstruturaDeRepeticaoEntregar/Exercicio1.java
@@ -0,0 +1,19 @@
+package ExerciciosEstruturaDeRepeticaoEntregar;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        for(int i = 0; i <=100; i++){
+            System.out.println("Mostrando na tela a sequência com for: " + i);
+        }
+
+        System.out.println("[----------------------------------------]");
+
+
+        int i = 0;
+
+        while (i <= 100){
+            System.out.println("Mostrando na tela a sequência com while: " + i);
+            i++;
+        }
+    }
+}
