1fd5f7a1c7a41a3ef654f0c725c0d71862b442b3
guiijanuario <guilherme.januario@zup.com.br>
2023-05-26 17:32:13 -0300

    Exericios 5 finalizado - Exericios da Aula

---
+++ src/ExerciciosEstruturaDeRepeticaoAula/Exercicio5.java
@@ -0,0 +1,11 @@
+package ExerciciosEstruturaDeRepeticaoAula;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        System.out.println("Você está na posição 101 da fila, aguarde a sua vez");
+        for(int i = 100; i >= 1; i--){
+            System.out.println("Posição da fila: " + i);
+        }
+        System.out.println("Parabéns chegou a sua vez!");
+    }
+}
