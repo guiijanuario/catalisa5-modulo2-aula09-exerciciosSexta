03bfeee2c38fceb127ef26a88bcdfb46543c43a8
guiijanuario <guilherme.januario@zup.com.br>
2023-05-26 16:03:22 -0300

    Subindo projeto e exericio 1 dos exercicios da aula

---
+++ src/ExerciciosEstruturaDeRepeticaoAula/Exercicio1.java
@@ -0,0 +1,11 @@
+package ExerciciosEstruturaDeRepeticaoAula;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+
+        for(int i = 0; i <=100; i++){
+            System.out.println("Mostrando na tela a sequência: " + i);
+        }
+
+    }
+}
