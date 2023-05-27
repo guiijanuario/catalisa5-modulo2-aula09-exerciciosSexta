aee6bedcac3b048b50db11d07338e1bfa959274a
guiijanuario <guilherme.januario@zup.com.br>
2023-05-26 23:40:06 -0300

    Exercicio 7 Finalizado - Exercicios de Repetição - Entregar

--- src/ExerciciosEstruturaDeRepeticaoEntregar/Exercicio7.java
+++ src/ExerciciosEstruturaDeRepeticaoEntregar/Exercicio7.java
@@ -9,14 +9,19 @@ public class Exercicio7 {
         System.out.println("[---------- Ver se o número é primo ----------]");
         System.out.println("Insira o número: ");
         int numero = sc.nextInt();
+        int quantidadeDeDivisor = 0;
 
-        for (int i = 2; i <= Math.sqrt(numero); i++){
-            if (numero % i == 1){
-                System.out.println("O número " + numero + " é primo!");
-            }else {
-
+        for(int i = 1; i <= numero; i++){
+            if(numero % i == 0){
+                quantidadeDeDivisor++;
+                System.out.println("O i é:" + i);
             }
-            // ainda não consegui fazer esse exercício.
+        }
+
+        if(quantidadeDeDivisor == 2){
+            System.out.println("Ele é um número primo! ");
+        } else {
+            System.out.println("Ele não é um número primo! ");
         }
     }
 }
