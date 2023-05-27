eacf3560910864ee2c189483b96aa1ec95c9fc1d
guiijanuario <guilherme.januario@zup.com.br>
2023-05-26 21:02:53 -0300

    Exercicio 7 ainda não finalizado,o

---
+++ src/ExerciciosEstruturaDeRepeticaoEntregar/Exercicio7.java
@@ -0,0 +1,22 @@
+package ExerciciosEstruturaDeRepeticaoEntregar;
+
+import java.util.Scanner;
+
+public class Exercicio7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("[---------- Ver se o número é primo ----------]");
+        System.out.println("Insira o número: ");
+        int numero = sc.nextInt();
+
+        for (int i = 2; i <= Math.sqrt(numero); i++){
+            if (numero % i == 1){
+                System.out.println("O número " + numero + " é primo!");
+            }else {
+                System.out.println("O número " + numero + " não é primo!");
+            }
+            // ainda não consegui fazer esse exercício.
+        }
+    }
+}
